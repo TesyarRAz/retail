20a7b5ecf92ece868c92f612695265b336d36afc
TesyarRAz <mohamadtesyar@gmail.com>
2021-11-08 16:45:56 +0700

    asd

--- app/Http/Controllers/Admin/TransaksiController.php
+++ app/Http/Controllers/Admin/TransaksiController.php
@@ -21,9 +21,11 @@ class TransaksiController extends Controller
             return datatables()->of(
                 Transaksi::select('transaksis.*')
                 ->where(fn($query) => 
-                    $query->where('jenis', 'diambil')->whereNotNull('bukti_transaksi')
+                    $query->->where(fn($query) => 
+                        $query->where('jenis', 'diambil')->whereNotNull('bukti_transaksi')
+                    )
+                    ->orWhere('jenis', 'dikirim')
                 )
-                ->orWhere('jenis', 'dikirim')
                 ->with('details', 'user')
                 ->when($request->has('kategori_id') && $request->kategori_id != -1, fn($query) =>
                     $query->whereHas('details.produk', fn($query) => $query->where('kategori_id', $request->kategori_id))
