58530b66a7f5fcee9c9b5b08f551d67adf4b39b6
TesyarRAz <mohamadtesyar@gmail.com>
2021-11-08 22:51:49 +0700

    asd

--- resources/views/admin/kategori/edit.blade.php
+++ resources/views/admin/kategori/edit.blade.php
@@ -15,11 +15,11 @@
 					<label>Nama <span class="font-weight-bold text-danger">*</span></label>
 					<input type="text" name="name" class="form-control" required>
 				</div>
-			</div>
-			<div class="form-group" data-toggle="image-preview">
-				<label>Gambar <span class="text-danger">*</span></label>
-				<input type="file" name="gambar" class="d-none" data-source="true" accept="image/*">
-				<img src="{{ asset('assets/images/empty-image.png') }}" role="button" class="d-block img-thumbnail" width="300" height="300" data-target="true">
+				<div class="form-group" data-toggle="image-preview">
+					<label>Gambar <span class="text-danger">*</span></label>
+					<input type="file" name="gambar" class="d-none" data-source="true" accept="image/*">
+					<img src="{{ asset('assets/images/empty-image.png') }}" role="button" class="d-block img-thumbnail" width="300" height="300" data-target="true">
+				</div>
 			</div>
 			<div class="modal-footer justify-content-between">
 				<button type="submit" class="btn btn-primary">Simpan</button>
